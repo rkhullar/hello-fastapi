a8d9c3c7a62ad685e149312cdd17b76c779e053b
Rajan Khullar <rkhullar03@gmail.com>
2021-08-22 20:16:55 -0400

    reorg

--- fastapi/api/v1/__init__.py
+++ fastapi/api/v1/__init__.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
-from . import items, login, users, misc
+from . import items, login, users
 
 router = APIRouter()
 router.include_router(login.router, tags=['login'])
 router.include_router(users.router, prefix='/users', tags=['users'])
 router.include_router(users.router, prefix='/items', tags=['items'])
-router.include_router(misc.router, prefix='/misc', tags=['misc'])
--- fastapi/api/v1/login.py
+++ fastapi/api/v1/login.py
@@ -1,3 +1,49 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from typing import Optional
+from jose import jwt
+import datetime as dt
+import os
+
+SECRET_KEY = os.environ['SECRET_KEY']
+SESSION_DURATION = 30  # minutes
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='./api/v1/login')
 
 router = APIRouter()
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+
+def fake_decode_token(token: str) -> User:
+    return User(username=f'test-{token}', email='noreply@example.com', full_name='Test User')
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
+
+
+def create_access_token(data: dict, duration: dt.timedelta, expiration_key: str = 'exp') -> str:
+    expiration = dt.datetime.utcnow() + duration
+    to_encode = dict(data, **{expiration_key: expiration})
+    return jwt.encode(claims=to_encode, key=SECRET_KEY, algorithm='HS256')
+
+
+@router.post('/login')
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = User(username=form_data.username)
+    access_token = create_access_token(data={'sub': f'username:{user.username}'},
+                                       duration=dt.timedelta(minutes=SESSION_DURATION))
+    return {'access_token': access_token, 'token_type': 'bearer'}
+
+
+@router.get('/me')
+async def read_current_user(current_user: User = Depends(get_current_user)):
+    return current_user
