712f350df181f523f8b0418c5cbe538184a37ff7
Rajan Khullar <rkhullar03@gmail.com>
2021-08-23 20:58:41 -0400

    saving work; added fake user with hashed password

--- fastapi/api/v1/login.py
+++ fastapi/api/v1/login.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from typing import Optional
 from jose import jwt
 import datetime as dt
 import os
+from util.hash import build_json_hash
 
 SECRET_KEY = os.environ['SECRET_KEY']
 SESSION_DURATION = 30  # minutes
@@ -19,6 +20,27 @@ class User(BaseModel):
     email: Optional[str] = None
     full_name: Optional[str] = None
     disabled: Optional[bool] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+    salt: str
+
+
+fake_users_db = {
+    'person': UserInDB(username='person', full_name='Person Test', email='noreply@example.com', hashed_password='vALXtyEJwIc8BK4Rqm8pi7NXJ6WWlitZdD78JanTOQkK7UPcRpEJL3YQt6PL7nXDtcxGg2NqkEP7DgVF7L5ACQ==', salt='bFIP5irOtSG7Jreu')
+}
+
+
+def verify_password(plain_password: str, hashed_password: str, salt: str) -> bool:
+    return build_json_hash(data={'password': plain_password}, salt=salt) == hashed_password
+
+
+def authenticate_user(db: dict, username: str, password: str) -> Optional[UserInDB]:
+    if username in db:
+        user = db[username]
+        if verify_password(plain_password=password, hashed_password=user.hashed_password, salt=user.salt):
+            return user
 
 
 def fake_decode_token(token: str) -> User:
@@ -38,7 +60,9 @@ def create_access_token(data: dict, duration: dt.timedelta, expiration_key: str 
 
 @router.post('/login')
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = User(username=form_data.username)
+    user = authenticate_user(fake_users_db, username=form_data.username, password=form_data.password)
+    if not user:
+        raise HTTPException(status_code=401)
     access_token = create_access_token(data={'sub': f'username:{user.username}'},
                                        duration=dt.timedelta(minutes=SESSION_DURATION))
     return {'access_token': access_token, 'token_type': 'bearer'}
