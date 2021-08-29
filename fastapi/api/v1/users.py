b3d47a0ce889912ec172c1ffe763128b1a0e58e5
Rajan Khullar <rkhullar03@gmail.com>
2021-08-29 17:45:47 -0400

    factory pattern and first mongo atlas collection (#3)

--- fastapi/api/v1/users.py
+++ fastapi/api/v1/users.py
@@ -1,3 +1,11 @@
 from fastapi import APIRouter
 
+from ..model import User as UserInDB
+from .login import User
+
 router = APIRouter()
+
+
+@router.get('/test', response_model=User)
+async def test_user():
+    return UserInDB.get(username='person')
---
+++ fastapi/config.py
@@ -0,0 +1,36 @@
+import os
+from typing import List
+
+from pydantic import BaseSettings
+
+
+class ProjectSettings(BaseSettings):
+    project: str = os.getenv('PROJECT', 'hello-fastapi')
+    environment: str = os.getenv('ENVIRONMENT')
+    debug = bool(os.getenv('DEBUG', 0))
+
+
+class NetworkSettings(BaseSettings):
+    service_host: str = os.getenv('SERVICE_HOST', '0.0.0.0')
+    service_port: int = int(os.getenv('SERVICE_PORT', '8000'))
+
+
+class SecuritySettings(BaseSettings):
+    secret_key: str = os.getenv('SECRET_KEY')
+
+
+class MongoSettings(BaseSettings):
+    mongo_db_names: List[str] = ['default']
+    mongo_host: str = os.getenv('MONGO_HOST')
+    mongo_username: str = os.getenv('MONGO_USERNAME')
+    mongo_password: str = os.getenv('MONGO_PASSWORD')
+    mongo_scheme: str = 'mongodb+srv'
+
+    @property
+    def mongo_uri(self) -> str:
+        base_uri = f'{self.mongo_scheme}://{self.mongo_username}:{self.mongo_password}@{self.mongo_host}'
+        return f'{base_uri}/default?retryWrites=true&w=majority'
+
+
+class Settings(ProjectSettings, NetworkSettings, SecuritySettings, MongoSettings):
+    pass
