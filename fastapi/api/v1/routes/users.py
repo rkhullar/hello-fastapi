b04ebd44e1a48577e6f842e860f312413cec0b96
Rajan Khullar <rkhullar03@gmail.com>
2021-09-07 21:44:55 -0400

    try security

--- fastapi/api/v1/routes/users.py
+++ fastapi/api/v1/routes/users.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Security
+from typing import List
 
 from ...model import User as UserInDB
 from ..schema import User
+from ..depends import get_current_active_user
 
 router = APIRouter()
 
 
-@router.get('/test', response_model=User)
-async def test_user():
-    return UserInDB.get(username='person')
+@router.get('/', response_model=List[User])
+async def list_users(current_user: User = Security(get_current_active_user, scopes=['hello'])):
+    return [UserInDB.get(username='groundx14')]
