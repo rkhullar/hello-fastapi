e62cafe1f5176395191507fb7cfaac415325ba27
Rajan Khullar <rkhullar03@gmail.com>
2021-09-09 09:38:05 -0400

    extend route logic; improve list logic

--- fastapi/api/core/router.py
+++ fastapi/api/core/router.py
@@ -2,5 +2,13 @@ from fastapi import APIRouter as DefaultRouter
 
 
 class APIRouter(DefaultRouter):
-    pass
+    def api_route(self, *args, **kwargs):
+        parent_decorator = super().api_route(*args, **kwargs)
 
+        def decorator(fn):
+            parent_decorator(fn)
+            fn.api_route = self.routes[-1]
+            assert fn.__qualname__ == fn.api_route.endpoint.__qualname__
+            return fn
+
+        return decorator
--- fastapi/api/v1/routes/users.py
+++ fastapi/api/v1/routes/users.py
@@ -1,7 +1,6 @@
-from typing import Callable, List, Optional
+from typing import List
 
 from fastapi import Depends, HTTPException
-from fastapi.routing import APIRoute
 
 from ...core.router import APIRouter
 from ...core.util import TokenData
@@ -18,32 +17,25 @@ class AllowedScopes:
         self.scopes = scopes
 
     def __call__(self, token_data: TokenData = Depends(get_token_data)):
-        # print(self.scopes)
-        # print(token_data)
+        # TODO: verify logic
         if not set(self.scopes).intersection(token_data.scopes):
             raise HTTPException(status_code=403, detail='forbidden')
 
 
-def find_route_for(router: APIRouter, fn: Callable) -> Optional[APIRoute]:
-    for route in router.routes:
-        if route.endpoint == fn:
-            return route
-
-
-def allowed_scopes(router: APIRouter, *scopes: str):
+def allowed_scopes(*scopes: str):
     def decorator(fn):
-        route = find_route_for(router, fn)
-        route.dependencies.append(Depends(AllowedScopes(*scopes)))
+        dependency = Depends(AllowedScopes(*scopes))
+        fn.api_route.dependencies.append(dependency)
         return fn
     return decorator
 
 
-@router.get('/', response_model=List[User], dependencies=[Depends(AllowedScopes('admin'))])
+@router.get('/1', response_model=List[User], dependencies=[Depends(AllowedScopes('admin'))])
 async def list_users_1():
-    return [UserInDB.get(username='groundx14')]
+    return list(UserInDB.objects.all())
 
 
-@allowed_scopes(router, 'admin')
+@allowed_scopes('admin')
 @router.get('/2', response_model=List[User])
 async def list_users_2():
-    return [UserInDB.get(username='groundx14')]
+    return list(UserInDB.objects.all())
