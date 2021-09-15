0de5ea2fb764aa75df7a8e4a446db35ea4ad8bff
Rajan Khullar <rkhullar03@gmail.com>
2021-09-15 11:16:50 -0400

    setup nosetests (#6)

--- fastapi/api/core/util/__init__.py
+++ fastapi/api/core/util/__init__.py
@@ -1,3 +1,4 @@
 from .hash import HashFactory
 from .jwt import TokenData, TokenFactory
 from .mongo import document_extras
+from .request import BearerAuth
--- fastapi/api/core/util/mongo.py
+++ fastapi/api/core/util/mongo.py
@@ -1,13 +1,15 @@
+from typing import Type
+
 from mongoengine import Document
 from mongoengine.errors import DoesNotExist
 from pymongo.collection import Collection
 
 
-def _build_extra(kls):
+def _build_extra(kls: Type[Document]):
     class DocumentExtras:
 
         @classmethod
-        def get(cls, raise_error: bool = False, **query) -> Document:
+        def get(cls, raise_error: bool = False, **query) -> kls:
             try:
                 return kls.objects.get(**query)
             except DoesNotExist as err:
@@ -22,11 +24,17 @@ def _build_extra(kls):
             # TODO: try to make into class property
             return kls._get_collection()
 
+        @classmethod
+        def create(cls, *args, **kwargs) -> kls:
+            doc = kls(*args, **kwargs)
+            doc.save()
+            return doc
+
     return DocumentExtras
 
 
-def document_extras(cls):
+def document_extras(cls: Type[Document]):
     to_inject = _build_extra(cls)
-    for member in ['get', 'dict', 'collection']:
+    for member in ['get', 'dict', 'collection', 'create']:
         setattr(cls, member, getattr(to_inject, member))
     return cls
